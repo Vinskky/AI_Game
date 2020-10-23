a15bd3e158c20428a07612bac4168fdfdcd8c305
Vinskky <naitsabesdv@gmail.com>
2020-10-23 19:10:04 +0200

    Turret Rotation
    Now tanks aim at eachother :)

---
+++ BaseProject_TANKS/Assets/Scripts/Tank/TurretRot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretRot : MonoBehaviour
+{
+    private GameObject target;
+    private float rotationSpeed;
+
+    private Quaternion lookRotation;
+    private Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] gos = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[]; //will return an array of all GameObjects in the scene
+        foreach (GameObject go in gos)
+        {
+            if (go.layer == 9 && go != transform.parent.parent.gameObject && go.name != "FireTransform")
+            {
+                target = go.gameObject;
+            }
+        }
+
+        rotationSpeed = 1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = (target.transform.position - transform.position).normalized;
+
+        lookRotation = Quaternion.LookRotation(direction);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+    }
+}
