a44ea78f06ba1536ebabf477d3a5e29c7ad6164c
EnricGDV <minenime4thewin@gmail.com>
2020-11-21 14:14:23 +0100

    Tank1 Behaviour Conditions

---
+++ BaseProject_TANKS/Assets/BehaviorBricks/Conditions/System/MyConditions/AreThereObstacles.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+using Pada1.BBCore.Framework;
+using Pada1.BBCore;
+
+[Condition("MyConditions/Are There Obstacles ?")]
+[Help("Checks whether there are obstacles between this tank and the enemy through a raycast.")]
+public class AreThereObstacles : ConditionBase
+{
+    public override bool Check()
+    {
+
+        GameObject enemy = GameObject.Find("Enemy");
+        GameObject agent = GameObject.Find("Agent");
+
+        RaycastHit ray;
+
+        Vector3 origin = agent.transform.position;
+        origin.y = 2;
+
+        Vector3 final = enemy.transform.position;
+        final.y = 2;
+
+        Vector3 direction = new Vector3(0.0f, 0.0f, 0.0f);
+        direction = final - origin;
+        direction.Normalize();
+
+        Physics.Raycast(origin, direction, out ray, Mathf.Abs(Vector3.Distance(origin, final)));
+        Debug.DrawLine(origin, final, Color.red);
+
+        return ray.collider == null;
+    }
+}
---
+++ BaseProject_TANKS/Assets/BehaviorBricks/Conditions/System/MyConditions/IsEnemyNear.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+using Pada1.BBCore.Framework;
+using Pada1.BBCore;
+
+[Condition("MyConditions/Is Enemy Near?")]
+[Help("Checks whether another tank is near this one.")]
+public class IsEnemyNear : ConditionBase
+{
+    ///<value>Input First Boolean Parameter.</value>
+    [InParam("Gravity")]
+    [Help("Gravity of the world")]
+    public float gravity;
+
+    ///<value>Input Second Boolean Parameter.</value>
+    [InParam("Launch Force")]
+    [Help("Second value to be compared")]
+    public float launchForce;
+
+    public override bool Check()
+    {
+        GameObject enemy = GameObject.Find("Enemy");
+        GameObject agent = GameObject.Find("Agent");
+        return Mathf.Abs(Vector3.Distance(enemy.transform.position, agent.transform.position)) < ((launchForce * launchForce) / gravity);
+    }
+}
